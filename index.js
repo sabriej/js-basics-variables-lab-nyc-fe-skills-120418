d5ad79058b2cb3c2f1ebb4fb7971acee7ddde045
Jennifer Gaillard <jengaillard1@gmail.com>
2019-01-16 00:25:41 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-// Code your solution in this file!
+expect(companyName).to.equal('Scuber');
+expect(function () { companyName = 'specialCompany' }).to.throw(TypeError);
+expect(mostProfitableNeighborhood).to.equal('Chelsea');
+expect(function () { mostProfitableNeighborhood = 'Upper West Side' }).to.not.throw(TypeError);
+expect(companyCeo).to.equal('Susan Smith');
+expect(function () { companyCeo = 'Lauren Hart' }).to.not.throw(TypeError);
