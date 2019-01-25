7b1cdab93661d17821d6a57d04962b1d083bb62d
Jennifer Gaillard <jengaillard1@gmail.com>
2019-01-25 02:48:21 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,3 @@
 const companyName = 'Scuber';
 let mostProfitableNeighborhood = 'Chelsea';
-// expect(function () { mostProfitableNeighborhood = 'Upper West Side' }).to.not.throw(TypeError);
-// let companyCeo = 'Susan Smith';
-// expect(function () { companyCeo = 'Lauren Hart' }).to.not.throw(TypeError);
-// });
+let companyCeo = 'Susan Smith';
