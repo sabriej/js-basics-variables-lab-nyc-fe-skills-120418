758cf8458ba8e4418273c98d60cefee6cae4f266
Jennifer Gaillard <jengaillard1@gmail.com>
2019-01-25 02:43:16 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-let companyName = 'Schuber';
-expect(function () { companyName = 'specialCompany' }).to.throw(TypeError);
-let mostProfitableNeighborhood = 'Chelsea';
-expect(function () { mostProfitableNeighborhood = 'Upper West Side' }).to.not.throw(TypeError);
-let companyCeo = 'Susan Smith';
-expect(function () { companyCeo = 'Lauren Hart' }).to.not.throw(TypeError);
-});
+// // let companyName = 'Schuber';
+// expect(function () { companyName = 'specialCompany' }).to.throw(TypeError);
+// let mostProfitableNeighborhood = 'Chelsea';
+// expect(function () { mostProfitableNeighborhood = 'Upper West Side' }).to.not.throw(TypeError);
+// let companyCeo = 'Susan Smith';
+// expect(function () { companyCeo = 'Lauren Hart' }).to.not.throw(TypeError);
+// });
