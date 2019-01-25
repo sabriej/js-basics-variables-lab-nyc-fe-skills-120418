3cb5f52484218d7da803ff39cc8c295fd64f9796
Jennifer Gaillard <jengaillard1@gmail.com>
2019-01-25 02:48:44 +0000

    Done.

--- index.js
+++ index.js
@@ -1,7 +1,3 @@
-let companyName = 'Schuber';
-expect(function () { companyName = 'specialCompany' }).to.throw(TypeError);
+const companyName = 'Scuber';
 let mostProfitableNeighborhood = 'Chelsea';
-expect(function () { mostProfitableNeighborhood = 'Upper West Side' }).to.not.throw(TypeError);
 let companyCeo = 'Susan Smith';
-expect(function () { companyCeo = 'Lauren Hart' }).to.not.throw(TypeError);
-});
